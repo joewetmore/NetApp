dcbb2766723421ffdde15dc52f6fe5126d08425b
JoeWetmore <25187551+joewetmore@users.noreply.github.com>
2024-05-01 15:35:57 -0600

    Create snapcenter50.ps1

---
+++ snapcenter50.ps1
@@ -0,0 +1,12 @@
+https://library.netapp.com/ecm/ecm_download_file/ECMLP2886895
+
+Open-SmStorageConnection 
+Add-SmStorageConnection -Storage test_vs1 -Protocol Https -Timeout 60 
+
+New-SdLun [-StorageSystem] <String> [-LunPath] <String> [-Size] <String> [[-Type] <String>] [-Session <String>] [-Host <String>] [<CommonParameters>] 
+
+New-SdStorage [-Path <String>] -Size <String> -LunPath <String> -StorageSystem <String> [-FileSystemLabel <String>] [-SharedDisk] [-ClusteredSharedVolume] 
+[-Igroup <String>] [-InitiatorInfo <HostInitiatorInfo>] [-PortSet <String>] [-AutopickMountPoint] [PartitionStyle <PartitionStyle>] [-ResourceGroup <String>] 
+[-Thin] [-AllocationUnitSize <String>] [-RawDeviceMapping] [-Datastore <String>] [-Session <String>] [-Host <String>] [<CommonParameters>] 
+
+
