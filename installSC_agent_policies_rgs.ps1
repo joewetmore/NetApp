6196e685eccab638218dc14d34ef2007432ac3b7
JoeWetmore <25187551+joewetmore@users.noreply.github.com>
2024-07-30 16:20:24 -0600

    Update and rename installSCagents.ps1 to installSC_agent_policies_rgs.ps1

--- installSC_agent_policies_rgs.ps1
+++ installSC_agent_policies_rgs.ps1
@@ -1,3 +1,9 @@
+ # Verify administrator
+if ( -not (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).
+    IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) ) {
+    throw 'Run as an administrator'
+}
+
 #open a connection to the SnapCenter server
 open-smconnection -smsbaseurl https://c1ssc01.ccp1.gene.com:8146
 
@@ -42,3 +48,27 @@ Install-SmHostPackage -hostnames @("sqlChost1.ccp1.gene.com", "sqlChost2.ccp1.ge
 Set-SmPluginConfiguration -PluginCode SCSQL -HostName "sqlClusterName".ccp1.gene.com -HostLogFolders @{"Host"="sqlChost1.ccp1.gene.com";"Log Folder"="E:\SCLOGS"},@{"Host"="sqlChost2.ccp1.gene.com";"Log Folder"="E:\SCLOGS"} -Confirm:$false
 
 
+$PolicyName = "12h_backup_"
+
+Add-SmPolicy `
+  -PolicyName 12h_backup_ `
+  -PolicyType Backup `
+  -PluginPolicyType SMSQL `
+  -RetentionSettings @{"BackupType"="DATA";"ScheduleType"="DAILY";"RetentionCount"='30'} `
+  -Description 'SQL daily schedule policy' `
+  -UtmType DayBase `
+  -UtmDays 7 `
+  -SqlBackupType FullBackupAndLogBackup `
+  -VerifyLogBackup $true `
+  -ScheduleType Daily 
+
+
+  Add-SmResourceGroup `
+  -ResourceGroupName "RG_TESTSQL2_12H" `
+  -PluginCode SCSQL `
+  -Resources @{"Host"="sqltest01.ccp1.gene.com";"Type"="SQL Instance";"Names"="sqltest01"} `
+  -Policies "12h_backup_" `
+  -VerificationServers "sqltest01" `
+  -VerificationSchedules @{"VerificationType"="VERIFY_AFTER_BACKUP";"VerificationServers"="sqltest01";"BackupPolicyName"="12h_backup_";"ScheduleType"="daily";"BackupScheduleType"="daily"} `
+  -Schedules @{"PolicyName"="12h_backup_";"ScheduleType"="Daily";"StartTime"=" 07/30/2024 4:00PM";"daysInterval"="1"} 
+ 
